142fb36df7d98ffc68f1b3d2bfb25c4a5b531032
Connor Pickles <cpickles98@gmail.com>
2018-12-30 00:36:34 -0400

    Now sets playlist image, if selected
    Takes base 64 image data that's hard coded into the program and uses it 
    to set the image for a playlist. Updated presets with this new 
    capability.

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -46,7 +46,7 @@ app.get('/login', function(req, res) {
     res.cookie(stateKey, state);
 
     // your application requests authorization
-    var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify playlist-modify-private';
+    var scope = 'user-read-private user-read-email playlist-read-private playlist-read-collaborative playlist-modify-public playlist-modify playlist-modify-private ugc-image-upload';
     res.redirect('https://accounts.spotify.com/authorize?' +
         querystring.stringify({
             response_type: 'code',
